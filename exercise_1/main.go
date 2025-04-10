0083e57e1ab31d677896cc832fe00809bf9c3dd0
CameronBibb <cameron.bibb@icloud.com>
2025-04-10 11:27:20 -0400

    Implement timer functionality for quiz and update scoring message

--- exercise_1/main.go
+++ exercise_1/main.go
@@ -6,11 +6,14 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question, answer'")
 	flag.Parse()
+
+	//implement change duration with flag
 
 	file, err := os.Open(*csvFilename)
 	if err != nil {
@@ -23,8 +26,17 @@ func main() {
 		exit("Failed to parse the provided CSV file.")
 	}
 	problems := parseLines(lines)
-
 	correct := 0
+
+	duration := 5
+
+	//implement start timer and quiz by pressing return
+
+	fmt.Println("Starting timer")
+	time.AfterFunc(time.Duration(duration)*time.Second, func() {
+		fmt.Printf("Time is up. You scored %d out of %d.\n", correct, len(problems))
+		os.Exit(0)
+	})
 
 	for i, p := range problems {
 		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
@@ -34,7 +46,7 @@ func main() {
 			correct++
 		}
 	}
-	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+	fmt.Printf("Quiz completed. You scored %d out of %d.\n", correct, len(problems))
 }
 
 func parseLines(lines [][]string) []problem {
@@ -57,3 +69,10 @@ func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
 }
+
+//create a timer with a default duration of 30 seconds (timer will start once created)
+//	timer duration is customizable with a flag
+//stop the quiz when the timer is done
+//stop the timer if the quiz is completed before timer
+
+//user should be prompted to hit the enter (or some key) to start the timer and the quiz: "Press enter to begin."
